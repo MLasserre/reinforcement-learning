f163b2ba64bb7ecde937122c798d26f4a2736c21
Marvin Lasserre <marvin.lasserre@lip6.fr>
2020-09-03 23:12:42 +0200

    Adding non-stationary bandit

--- chapter2/k_bandit.py
+++ chapter2/k_bandit.py
@@ -6,24 +6,49 @@ import matplotlib.pyplot as plt
 mpl.rc('text', usetex=True)
 mpl.rc('font', family='serif')
 
+def normal_initialization(k, mu=0., sigma=1.):
+    return np.random.normal(mu, sigma, size=k)
+
+def constant_initialization(k, c=0.):
+    return np.full(k,c)
+
 class bandit:
-    def __init__(self, k, mu=0., sigma=1.):
+    def __init__(self, k, q, mode='s'):
+        # Mode is 's' for stationary or 'ns' for non-stationary
+        # Maybe we could pass an update function as parameter ?
         self.__k = k
-        self.__q = np.random.normal(mu, sigma, size=k)
+        self.__q = q
         self.__ba = np.argmax(self.__q) # Best action
+        self.__mode = mode
 
     def computeReward(self, A):
         # The action must be an integer
         # such that 0 <= A <= k - 1
         if (A < 0) or (A >= k): 
             return # Add exception
-        return np.random.normal(loc=self.__q[A])
+        reward = np.random.normal(loc=self.__q[A])
+        self.__updateValues()
+        self.__updateBestAction()
+        return reward
 
     def getBestAction(self):
         return self.__ba
 
     def getNActions(self):
         return self.__k
+
+    def __updateValues(self):
+        if self.__mode == 's':
+            return # Do nothing
+        elif self.__mode == 'ns':
+            self.__q += np.random.normal(scale=0.01, size=self.__k)
+            return
+        else:
+            return #Add an exception
+
+    def __updateBestAction(self):
+        return np.argmax(self.__q)
+
 
 class epsilon_greedy:
     def __init__(self, epsilon, n_step, bandit, debug=False):
@@ -76,6 +101,8 @@ class epsilon_greedy:
 
 
 if __name__ == "__main__":
+    save = False
+
     k = 10
     n_run = 2000
     n_time = 1000
@@ -83,14 +110,15 @@ if __name__ == "__main__":
     epsilons = [0., 0.1, 0.01]
     results = {}
 
-    oa = '\% Optimal action'
+    oa = '% Optimal action'
     ar = 'Average reward'
     
     for epsilon in epsilons:
         print("Epsilon: ", epsilon)
         runs = {oa:[], ar:[]}
         for i in range(n_run):
-            B = bandit(k)
+            q = constant_initialization(k) # True values
+            B = bandit(k, q, mode='ns')
             learner = epsilon_greedy(epsilon, n_time, B, debug=True)
             learner.learn()
             infos = learner.getInfo()
@@ -111,7 +139,10 @@ if __name__ == "__main__":
     ax.set_xlim([0,n_time])
     ax.set_xlabel('Steps')
     ax.legend()
-    fig.savefig("average_reward.pdf", transparent=True)
+    if save:
+        fig.savefig("average_reward.pdf", transparent=True)
+    else:
+        plt.show()
 
     fig, ax= plt.subplots()
     for epsilon in results:
@@ -123,4 +154,7 @@ if __name__ == "__main__":
     ax.set_ylim([0,100])
     ax.yaxis.set_major_formatter(mtick.PercentFormatter())
     ax.legend()
-    fig.savefig("optimal_action.pdf", transparent=True)
+    if save:
+        fig.savefig("optimal_action.pdf", transparent=True)
+    else:
+        plt.show()
